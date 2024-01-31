f8719c173cd193ac4acf0184f960ef24526e87d7
Vinicius0422 <guedescosta123@hotmail.com>
2024-01-31 00:50:29 -0300

    creating form component

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  standalone: true,
+  imports: [],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss'
+})
+export class FormComponent {
+
+}
